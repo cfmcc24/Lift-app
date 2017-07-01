57684796203e18fa8fec03f845e2ae08f66fa737
jpeterson <jpeterson@hubspot.com>
2017-07-01 13:56:55 -0400

    Baby's first embedded component

--- index.ios.js
+++ index.ios.js
@@ -1,46 +1,42 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
+import LiftsTable from './js/LiftsTable';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 
 export default class LiftApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Greetings Corey, My Son.
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<LiftsTable />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 AppRegistry.registerComponent('LiftApp', () => LiftApp);
---
+++ js/LiftsTable.js
@@ -0,0 +1,15 @@
+/**
+ * @flow
+ */
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class LiftsTable extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+export default LiftsTable;
