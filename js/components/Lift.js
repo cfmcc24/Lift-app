428b545021ce89ed287c5f7d000925cd6527ab87
jpeterson <jpeterson@hubspot.com>
2017-07-02 12:36:43 -0400

    Add basic containers and components

---
+++ js/components/Lift.js
@@ -0,0 +1,20 @@
+/**
+ * @flow
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Lift extends Component {
+	render() {
+		<li onClick={this.props.onClick}>
+			{this.props.text}
+		</li>
+	});
+}
+
+Lift.propTypes = {
+	onClick: PropTypes.func.isRequired,
+	text: PropTypes.string.isRequired
+}
+
+export default Lift
--- js/components/LiftsTable.js
+++ js/components/LiftsTable.js
@@ -2,14 +2,27 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types'
+import Lift from './Lift'
 
 class LiftsTable extends Component {
-  render() {
-    return (
-      <Text>Hello world!</Text>
-    );
-  }
+	render() {
+		<ul>
+			{ this.props.lifts.map(lift => (
+				<Lift key={lift.id} {...lift} onClick={() => onLiftClick(lift.id)} />
+			)) }
+		</ul>
+	});
+}
+
+LiftsTable.propTypes = {
+	lifts: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			text: PropTypes.string.isRequired
+		}).isRequired
+	).isRequired,
+	onLiftClick: PropTypes.func.isRequired
 }
 
 export default LiftsTable;
