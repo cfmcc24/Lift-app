e7f5b8ac42ffb250787384bbcdf560d63e1d60e1
jpeterson <jpeterson@hubspot.com>
2017-07-02 12:39:46 -0400

    Wire up containers; rename styles

--- index.ios.js
+++ index.ios.js
@@ -2,19 +2,21 @@
  * @flow
  */
 import React, { Component } from 'react';
-import LiftsTable from './js/components/LiftsTable';
+import LiftsListContainer from './js/containers/LiftsListContainer';
+import AddLift from './js/containers/AddLift';
 import {
   AppRegistry,
   Text,
   View
 } from 'react-native';
-import Styles from './js/styles/LiftsTable';
+import Styles from './js/styles/AppContainer';
 
 export default class LiftApp extends Component {
   render() {
     return (
       <View style={Styles.container}>
-        <LiftsTable />
+				<AddLift />
+        <LiftsListContainer />
       </View>
     );
   }
